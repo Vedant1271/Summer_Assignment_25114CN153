867def0341c2cec6d0828b3c222270514c19ed82
Vedant1271 <166494693+Vedant1271@users.noreply.github.com>
2026-06-30 15:07:17 +0530

    Add SumFunction class with sum method

---
+++ Day11/Q1.java
@@ -0,0 +1,10 @@
+public class SumFunction {
+
+    static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Sum = " + sum(10, 20));
+    }
+}
