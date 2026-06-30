79e58b054e3296fe258ddb8600e9f0d6204e0116
Vedant1271 <166494693+Vedant1271@users.noreply.github.com>
2026-06-30 15:16:22 +0530

    Create Q3.java

---
+++ Day14/Q3.java
@@ -0,0 +1,22 @@
+public class SecondLargest {
+
+    public static void main(String[] args) {
+
+        int[] arr = {10,50,30,80,60};
+
+        int largest = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] > largest) {
+                second = largest;
+                largest = arr[i];
+            } else if (arr[i] > second && arr[i] != largest) {
+                second = arr[i];
+            }
+        }
+
+        System.out.println("Second Largest = " + second);
+    }
+}
