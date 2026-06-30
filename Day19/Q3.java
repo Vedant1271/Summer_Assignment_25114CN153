2af1904a475b2575873d47b75b790b708362ba2f
Vedant1271 <166494693+Vedant1271@users.noreply.github.com>
2026-06-30 15:38:53 +0530

    Create Q3.java

---
+++ Day19/Q3.java
@@ -0,0 +1,19 @@
+public class MatrixTranspose {
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6}
+        };
+
+        System.out.println("Transpose:");
+
+        for (int i = 0; i < matrix[0].length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print(matrix[j][i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
