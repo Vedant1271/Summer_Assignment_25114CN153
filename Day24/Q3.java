35436121cb4acceaf4b44964d0d0dcf56f141f82
Vedant1271 <166494693+Vedant1271@users.noreply.github.com>
2026-06-30 15:59:28 +0530

    Implement LongestWord class to find longest word

---
+++ Day24/Q3.java
@@ -0,0 +1,20 @@
+public class LongestWord {
+
+    public static void main(String[] args) {
+
+        String str = "Java is a powerful programming language";
+
+        String[] words = str.split(" ");
+
+        String longest = "";
+
+        for (String word : words) {
+
+            if (word.length() > longest.length()) {
+                longest = word;
+            }
+        }
+
+        System.out.println("Longest Word = " + longest);
+    }
+}
