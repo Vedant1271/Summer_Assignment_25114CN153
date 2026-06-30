2b57c7fe4f1a68e80dec32187f051540fc9c7b4a
Vedant1271 <166494693+Vedant1271@users.noreply.github.com>
2026-06-30 15:58:33 +0530

    Add StringRotation class to check string rotations

---
+++ Day24/Q1.java
@@ -0,0 +1,15 @@
+public class StringRotation {
+
+    public static void main(String[] args) {
+
+        String str1 = "ABCD";
+        String str2 = "CDAB";
+
+        if (str1.length() == str2.length() &&
+            (str1 + str1).contains(str2)) {
+            System.out.println("String is Rotation");
+        } else {
+            System.out.println("String is Not Rotation");
+        }
+    }
+}
