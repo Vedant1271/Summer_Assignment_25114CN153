2d271aaf577cb8b505b6dc59ed342e1b101e21da
Vedant1271 <166494693+Vedant1271@users.noreply.github.com>
2026-06-30 15:23:48 +0530

    Add PairWithGivenSum class to find pairs in array

---
+++ Day16/Q3.java
@@ -0,0 +1,20 @@
+public class PairWithGivenSum {
+
+    public static void main(String[] args) {
+
+        int[] arr = {2, 4, 3, 5, 7, 8, 9};
+        int target = 9;
+
+        System.out.println("Pairs are:");
+
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i] + arr[j] == target) {
+                    System.out.println(arr[i] + " + " + arr[j] + " = " + target);
+                }
+            }
+        }
+    }
+}
