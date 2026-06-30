b394755f2fbb48a6bc581c9d3fd5f05d8d2f97bf
Vedant1271 <166494693+Vedant1271@users.noreply.github.com>
2026-06-30 13:38:50 +0530

    day 5 Q3

---
+++ Day5/Q3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class FactorsOfNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num;
+
+        System.out.print("Enter a number: ");
+        num = sc.nextInt();
+
+        System.out.println("Factors of " + num + " are:");
+
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                System.out.print(i + " ");
+            }
+        }
+
+        sc.close();
+    }
+}
